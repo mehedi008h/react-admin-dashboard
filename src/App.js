f23c7d26dd2bf95645216441ec4dca472b438d8f
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-13 21:17:03 +0600

    design new user & product page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formSource";
 
 function App() {
   return (
@@ -18,13 +19,19 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":id" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
             {/* nested routing for products*/}
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":id" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
