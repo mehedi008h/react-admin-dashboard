f054730d7645639254069367ff033763c06305a7
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-13 19:00:12 +0600

    design user list using mui datatable

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import List from "../../components/table/List";
 import Widget from "../../components/widget/Widget";
 import "./Home.scss";
 
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List />
         </div>
       </div>
     </div>
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,10 +1,17 @@
 import React from "react";
+import DataTable from "../../components/datatable/DataTable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./List.scss";
 
 const List = () => {
   return (
-    <div>
-      <h1>List</h1>
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable />
+      </div>
     </div>
   );
 };
